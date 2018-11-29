4a615d2f9e05c92b0930ca2f35ed42530685d8ff
Michael Woodruff <michael.woodruff@scmp.com>
2018-11-29 12:03:42 +0800

    REMOVE: Storyboard initial VC

--- TechChallenge/AppDelegate.swift
+++ TechChallenge/AppDelegate.swift
@@ -11,11 +11,16 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-  var window: UIWindow?
+  public var window: UIWindow?
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.makeKeyAndVisible()
+
+    window?.rootViewController = ViewController()
+
     return true
   }
 
