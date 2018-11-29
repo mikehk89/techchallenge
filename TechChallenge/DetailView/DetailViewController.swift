cf8fbc81b7474564b0d987c3233b65e262972f9b
Michael Woodruff <michael.woodruff@scmp.com>
2018-11-29 12:08:12 +0800

    UPDATED: Skeleton layout to project file

---
+++ TechChallenge/DetailView/DetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.swift
+//  TechChallenge
+//
+//  Created by Michael Woodruff on 29/11/2018.
+//  Copyright © 2018 Michael Woodruff. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailViewController: UIViewController {
+
+}
--- TechChallenge/IndexView/IndexViewController.swift
+++ TechChallenge/IndexView/IndexViewController.swift
@@ -8,14 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class IndexViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-    
   }
-
-
 }
 
