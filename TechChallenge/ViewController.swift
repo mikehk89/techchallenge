75c3e4126e7f1f683c8efb1f5ab4a824ed7b5667
Michael Woodruff <michael.woodruff@scmp.com>
2018-11-29 11:35:29 +0800

    Initial Commit

---
+++ TechChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechChallenge
+//
+//  Created by Michael Woodruff on 29/11/2018.
+//  Copyright © 2018 Michael Woodruff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
